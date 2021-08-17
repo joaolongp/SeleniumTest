0b9d8eeaa027ee6bcc0ef8f10224ce85c3e29390
João Longaray Pereira <joao.pereira@softdesign.com.br>
2021-08-17 14:39:43 -0300

    exec form

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -3,8 +3,11 @@ package base;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class BaseTests {
 
@@ -14,12 +17,23 @@ public class BaseTests {
     public static void setUp(){
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
         driver = new ChromeDriver();
+        driver.get("http://way2automation.com/way2auto_jquery/index.php");
+        driver.manage().window().maximize();
     }
 
     @Test
-    public void openRegisterWindowTest(){
-        driver.get("http://demo.automationtesting.in/Register.html");
-        Assert.assertEquals(driver.getTitle(), "Register");
-    }
+    public void fillAllFieldsTest() throws InterruptedException {
+        driver.findElement(By.name("name")).sendKeys("joao");
+        driver.findElement(By.name("phone")).sendKeys("1234567890");
+        driver.findElement(By.name("email")).sendKeys("joao@email.com");
+        driver.findElement(By.name("city")).sendKeys("Porto Alegre");
 
+        Select dropdown = new Select(driver.findElement(By.name("country")));
+        dropdown.selectByVisibleText("Brazil");
+
+        driver.findElement(By.xpath("//*[@id=\"load_form\"]/fieldset[6]/input")).sendKeys("joaouser");
+        driver.findElement(By.xpath("//*[@id=\"load_form\"]/fieldset[7]/input")).sendKeys("joaopassword");
+
+        driver.findElement(By.cssSelector("#load_form > div:nth-child(12) > div.span_1_of_4 > input")).click();
+    }
 }
