66c2cc6b84939885e09df6722bca12b1f7f9f873
João Longaray Pereira <joao.pereira@softdesign.com.br>
2021-08-18 18:55:16 -0300

    pageobjects and first tests

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,30 +1,39 @@
 package base;
 
-import org.junit.Assert;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
-
-import java.time.Duration;
 
 public class BaseTests {
 
     private static WebDriver driver;
 
     @BeforeClass
-    public static void setUp(){
-        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.get("http://way2automation.com/way2auto_jquery/index.php");
-        driver.manage().window().maximize();
+    public static void setupClass() {
+        WebDriverManager.chromedriver().setup();
     }
 
+    @Before
+    public void setupTest() {
+        driver = new ChromeDriver();
+    }
+
+//    @After
+//    public void teardown() {
+//        if (driver != null) {
+//            driver.quit();
+//        }
+//    }
+
     @Test
-    public void fillAllFieldsTest() throws InterruptedException {
+    public void fillAllFieldsTest() {
+        driver.get("http://way2automation.com/way2auto_jquery/index.php");
         driver.findElement(By.name("name")).sendKeys("joao");
         driver.findElement(By.name("phone")).sendKeys("1234567890");
         driver.findElement(By.name("email")).sendKeys("joao@email.com");
@@ -37,9 +46,5 @@ public class BaseTests {
         driver.findElement(By.xpath("//*[@id=\"load_form\"]/fieldset[7]/input")).sendKeys("joaopassword");
 
         driver.findElement(By.cssSelector("#load_form > div:nth-child(12) > div.span_1_of_4 > input")).click();
-
-        WebElement element = driver.findElement(By.xpath("//p[text()='This is just a dummy form, you just clicked SUBMIT BUTTON']"));
-
-        Assert.assertEquals(element.getText(), "This is just a dummy form, you just clicked SUBMIT BUTTON");
     }
 }
